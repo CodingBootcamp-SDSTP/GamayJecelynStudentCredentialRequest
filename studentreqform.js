3de843edfad1af4f24e82c3eec320cdf9005be75
Jecelyn <jecelyn1989@gmail.com>
2018-06-20 16:11:36 +0800

    login form

---
+++ studentreqform.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	var s = document.getElementById('sidebar');
+
+	s.onclick = function() {
+		var n = document.getElementById('box');
+		if (n.style.display != "none") {
+			n.style.display = "block";
+		}
+	}
+}
